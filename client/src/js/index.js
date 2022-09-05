bde0620939d2abecb5c5e44423ea13eab9b1c2d3
Betsy <betsy.griffith12@gmail.com>
2022-09-05 10:44:20 -0400

    added contact database with indexedb

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,17 +1,27 @@
+//import functions
+import { initdb } from './database';
+
+//import JS files
 import './form';
 import './submit';
+
+// Import CSS files
+import "../css/index.css";
+
+//import Bootstrap
 import { Tooltip, Toast, Popover } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//import images
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+//on load functionality
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
 });
 
-// Import CSS files
-import "../css/index.css";
